e8c661a6c3974161c99f68217d5fb240d39a5550
WalterJin2 <walterjin2@gmail.com>
2023-03-13 06:32:43 +1100

    Initial commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:2998",
+  "http://localhost:2999",
+  "https://www.dandrepairshop.com",
+  "https://dandrepairshop.com",
+];
+
+module.exports = allowedOrigins
