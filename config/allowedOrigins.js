c7ebed440c0d30a89eaa0a54412f2e672718df42
WalterJin2 <walterjin2@gmail.com>
2023-03-15 19:24:52 +1100

    update readme.md

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:2998',
   'http://localhost:2999',
-  'https://cityhospitallabmanagement.onrender.com',
+  'https://hospitallabmanager.onrender.com',
 ]
 
 module.exports = allowedOrigins
